f632baeb51969b1b5cf3855837afbfce174fc32b
ben-wycliff <ben12wycliff@gmail.com>
2021-12-15 13:48:58 +0300

    Booleans

---
+++ bin/boolean.dart
@@ -0,0 +1,7 @@
+import 'package:dart_intro/dart_intro.dart' as dart_intro;
+
+void main(List<String> arguments) {
+  bool isOn = true;
+  bool isMan = false;
+  print('Is on:  ${isOn}, Is man: ${isMan}!');
+}
