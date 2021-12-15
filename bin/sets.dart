ad63b869601cc7bb3567956a9ce870ce7b372ffd
ben-wycliff <ben12wycliff@gmail.com>
2021-12-15 14:01:44 +0300

    Sets

---
+++ bin/sets.dart
@@ -0,0 +1,12 @@
+main(List<String> arguments){
+  //sets are unordered and do not have duplicates
+
+  Set<int> numbers = new Set<int>();
+  numbers.add(1);
+  numbers.add(2);
+  numbers.add(2);
+  numbers.add(3);
+  numbers.add(3);
+  numbers.add(3);
+  print(numbers);
+}
