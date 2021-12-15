0a1063bcceea40bbf7f10f7a38cc068647a8c2c9
ben-wycliff <ben12wycliff@gmail.com>
2021-12-15 13:55:48 +0300

    Switch

---
+++ bin/switch.dart
@@ -0,0 +1,15 @@
+main(List<String> arguments){
+  int age = 18;
+
+  switch(age){
+    case 18:
+      print("teenager");
+      break;
+    case 20:
+      print("Young adult");
+      break;
+    default:
+      print("Not known");
+      break;
+  }
+}
